0d18a10afcfebbd09ddac1b6ace4e921faa6cbfe
dazzlerabhi30800 <70561836+dazzlerabhi30800@users.noreply.github.com>
2021-07-29 10:03:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+new hoverEffect({
+    parent : document.querySelector(".distortion"),
+    intensity : 0.2,
+    image1 : "./black.jpg",
+    image2 : "./black2.jpg",
+    // angle : Math.PI / 4,
+    speedIn : 3,
+    speedOut : 5,
+    displacementImage : "./diss.png"
+});
