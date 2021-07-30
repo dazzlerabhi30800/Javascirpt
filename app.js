bb6b466b0491cb42509cf180f630debd141bf9f0
dazzlerabhi30800 <70561836+dazzlerabhi30800@users.noreply.github.com>
2021-07-30 20:34:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click',()=>{
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+
+
+        // Animate Links
+        navLinks.forEach((links,index) => {
+            if(links.style.animation){
+                links.style.animation = "";
+            }
+            else{
+                
+                links.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.7}s`;
+            }
+            // console.log(index / 7);
+        });
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    });
+
+
+    
+
+}
+navSlide();
+
